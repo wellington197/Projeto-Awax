92a80bb9d73e0264447b64ecf83916e216f795c7
Francisco Wellington Costa <fcowellcosta@gmail.com>
2021-04-14 23:51:41 -0300

    Configuration of folder assets
    Configuration of folder assets

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 })
 
 //Public
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "assets")))
 
 //Launch listening server on port 8888
 const PORT = process.env.PORT || 8888
